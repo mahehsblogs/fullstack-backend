77a6513e2ddaf0669f64efeb5867c59a8d18ca54
Mahesh Kumar <maheshkumarnit96@gmail.com>
2023-03-24 22:08:17 +0530

    Initial commit

---
+++ src/main/java/com/MyCode/fullstack/backend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.MyCode.fullstack.backend.repository;
+
+import com.MyCode.fullstack.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ src/test/java/com/MyCode/fullstack/backend/FullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyCode.fullstack.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
