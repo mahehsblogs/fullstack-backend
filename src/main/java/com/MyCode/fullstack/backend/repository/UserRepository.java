bf19545aa2e1ea85c0993e06ae318fcc842f06c0
Mahesh Kumar <maheshkumarnit96@gmail.com>
2023-05-14 13:16:53 +0530

    udated comment

--- src/main/java/com/MyCode/fullstack/backend/repository/UserRepository.java
+++ src/main/java/com/MyCode/fullstack/backend/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.MyCode.fullstack.backend.repository;
 import com.MyCode.fullstack.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
+/*
  this is JPA repository Interface which is used to help in database interaction
  */
 
