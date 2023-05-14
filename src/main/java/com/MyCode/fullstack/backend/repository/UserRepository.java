5b1540bd2a18f3a6f7517eee8aad028dbc3159b8
Mahesh Kumar <maheshkumarnit96@gmail.com>
2023-05-14 13:12:21 +0530

    added massage

--- src/main/java/com/MyCode/fullstack/backend/repository/UserRepository.java
+++ src/main/java/com/MyCode/fullstack/backend/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.MyCode.fullstack.backend.repository;
 import com.MyCode.fullstack.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ this is JPA repository Interface which is used to help in database interaction
+ */
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
